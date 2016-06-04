cvelevet/navdtools

commit f072c3e94111857099b79daee9e752cdd08fec7e

navp: simple X-Plane plugin; creates "smart" braking-related commands.

Makefile
@@ -1,44 +1,61 @@
-SOURCE_DIR  = src
-NDCONV_EXE  = navdconv
-CFLAGS      = -O3 -std=c99
-TARGET_ARCH = -arch i386 -arch x86_64
-GITVERSION  = $(shell find . -name ".git" -type d -exec git describe --long --always --dirty=/m --abbrev=1 --tags \;)
+SOURCE_DIR = src
+SOURCE_SDK = XPSDK213
+NDCONV_EXE = navdconv
+NAVP_XPDLL = navP.xpl
+NDTINCLUDE = -I$(SOURCE_DIR)
+SDKINCLUDE = -I$(SOURCE_SDK)/CHeaders
+SDKLDPATHS = -F$(SOURCE_SDK)/Libraries/Mac
+SDKLDLINKS = -framework XPLM -framework XPWidgets
+CFLAGS     = -O3 -std=c99
+TARGETARCH = -arch i386 -arch x86_64
+GITVERSION = $(shell find . -name ".git" -type d -exec git describe --long --always --dirty=/m --abbrev=1 --tags \;)
 
-override CFLAGS      += -I$(SOURCE_DIR) -DNDCONV_EXE="\"$(NDCONV_EXE)\""
-override NDC_SOURCES  =   $(SOURCE_DIR)/tools/navdconv.c
-override NDC_OBJECTS  =   $(addsuffix .o,$(basename $(notdir $(NDC_SOURCES))))
-override LIB_HEADERS  =   $(wildcard $(SOURCE_DIR)/lib/*.h)
-override LIB_SOURCES  =   $(wildcard $(SOURCE_DIR)/lib/*.c)
-override LIB_OBJECTS  =   $(addsuffix .o,$(basename $(notdir $(LIB_SOURCES))))
-override COM_HEADERS  =   $(wildcard $(SOURCE_DIR)/common/*.h)
-override COM_SOURCES  =   $(wildcard $(SOURCE_DIR)/common/*.c)
-override COM_OBJECTS  =   $(addsuffix .o,$(basename $(notdir $(COM_SOURCES))))
-override CPT_HEADERS  =   $(SOURCE_DIR)/compat/compat.h
-override CPT_SOURCES  =   $(SOURCE_DIR)/compat/compat.c
-override CPT_OBJECTS  =   $(addsuffix .o,$(basename $(notdir $(CPT_SOURCES))))
-override WMM_HEADERS  =   $(wildcard $(SOURCE_DIR)/wmm/*.h)
-override WMM_SOURCES  =   $(wildcard $(SOURCE_DIR)/wmm/*.c)
-override WMM_OBJECTS  =   $(addsuffix .o,$(basename $(notdir $(WMM_SOURCES))))
+NDC_DEFINES = -DNDCONV_EXE="\"$(NDCONV_EXE)\""
+NDC_SOURCES = $(SOURCE_DIR)/tools/navdconv.c
+NDC_OBJECTS = $(addsuffix .o,$(basename $(notdir $(NDC_SOURCES))))
+LIB_HEADERS = $(wildcard $(SOURCE_DIR)/lib/*.h)
+LIB_SOURCES = $(wildcard $(SOURCE_DIR)/lib/*.c)
+LIB_OBJECTS = $(addsuffix .o,$(basename $(notdir $(LIB_SOURCES))))
+COM_HEADERS = $(wildcard $(SOURCE_DIR)/common/*.h)
+COM_SOURCES = $(wildcard $(SOURCE_DIR)/common/*.c)
+COM_OBJECTS = $(addsuffix .o,$(basename $(notdir $(COM_SOURCES))))
+CPT_HEADERS = $(SOURCE_DIR)/compat/compat.h
+CPT_SOURCES = $(SOURCE_DIR)/compat/compat.c
+CPT_OBJECTS = $(addsuffix .o,$(basename $(notdir $(CPT_SOURCES))))
+WMM_HEADERS = $(wildcard $(SOURCE_DIR)/wmm/*.h)
+WMM_SOURCES = $(wildcard $(SOURCE_DIR)/wmm/*.c)
+WMM_OBJECTS = $(addsuffix .o,$(basename $(notdir $(WMM_SOURCES))))
+NVP_XPLUGIN = -dynamiclib -fvisibility=hidden
+NVP_DEFINES = -DXPLM200 -DXPLM210 -DAPL=1 -DIBM=0 -DLIN=0
+NVP_HEADERS = $(wildcard $(SOURCE_DIR)/plugins/navP/*.h)
+NVP_SOURCES = $(wildcard $(SOURCE_DIR)/plugins/navP/*.c)
+NVP_OBJECTS = $(addsuffix .o,$(basename $(notdir $(NVP_SOURCES))))
 
-all: navdconv
+all: navdconv navp
+
+navp: nvpobj libobj comobj compat wmmobj
+	$(CC) $(NVP_XPLUGIN) $(SDKLDPATHS) $(SDKLDLINKS) $(SDKINCLUDE) $(NDTINCLUDE) $(NVP_DEFINES) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NAVP_XPDLL) $(NVP_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
+
+nvpobj: $(NVP_SOURCES) $(NVP_HEADERS)
+	$(CC) $(SDKINCLUDE) $(NDTINCLUDE) $(NVP_DEFINES) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(NVP_SOURCES)
 
 navdconv: ndcobj libobj comobj compat wmmobj
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGET_ARCH) -o $(NDCONV_EXE) $(NDC_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
+	$(CC) $(NDTINCLUDE) $(NDC_DEFINES) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NDCONV_EXE) $(NDC_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
 
 ndcobj: $(NDC_SOURCES)
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $(NDC_SOURCES)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(NDC_SOURCES)
 
 libobj: $(LIB_SOURCES) $(LIB_HEADERS)
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $(LIB_SOURCES)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(LIB_SOURCES)
 
 comobj: $(COM_SOURCES) $(COM_HEADERS)
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $(COM_SOURCES)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(COM_SOURCES)
 
 compat: $(CPT_SOURCES) $(CPT_HEADERS)
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $(CPT_SOURCES)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(CPT_SOURCES)
 
 wmmobj: $(WMM_SOURCES) $(WMM_HEADERS)
-	$(CC) $(CFLAGS) $(CPPFLAGS) $(TARGET_ARCH) -c $(WMM_SOURCES)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(WMM_SOURCES)
 
 linux:
 	$(MAKE) -f Makefile.linux
@@ -49,12 +66,13 @@ mingw:
 .PHONY: version
 version:
 ifneq ($(strip $(GITVERSION)),)
-override CFLAGS += -DNDT_VERSION="\"$(GITVERSION)\""
+NDC_DEFINES += -DNDT_VERSION="\"$(GITVERSION)\""
 endif
 
 .PHONY: clean
 clean:
 	@ $(MAKE) -f Makefile.linux clean
 	@ $(MAKE) -f Makefile.mingw clean
-	$(RM) $(NDCONV_EXE)
-	$(RM) $(NDC_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS)
+	$(RM) $(NDCONV_EXE)  $(NDC_OBJECTS)
+	$(RM) $(NAVP_XPDLL)  $(NVP_OBJECTS)
+	$(RM) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS)

Makefile.linux
@@ -1,9 +1,9 @@
-CFLAGS      = -O3 -std=gnu99 -Wno-unused-result -D_GNU_SOURCE
-LDLIBS      = -lm
-TARGET_ARCH =
+CFLAGS     = -O3 -std=gnu99 -Wno-unused-result -D_GNU_SOURCE
+LDLIBS     = -lm
+TARGETARCH =
 
 all:
-	$(MAKE) -f Makefile CFLAGS="$(CFLAGS)" LDLIBS="$(LDLIBS)" TARGET_ARCH="$(TARGET_ARCH)" navdconv
+	$(MAKE) -f Makefile CFLAGS="$(CFLAGS)" LDLIBS="$(LDLIBS)" TARGETARCH="$(TARGETARCH)" navdconv
 
 .PHONY: clean
 clean:

Makefile.mingw
@@ -1,10 +1,10 @@
-CC          = x86_64-w64-mingw32-gcc
-CFLAGS      = -O3 -std=gnu99 -DMINGW_HAS_SECURE_API -DCOMPAT_STRCASESTR=1 -DCOMPAT_STRERROR_R=1 -DCOMPAT_STRNDUP=1 -DCOMPAT_STRSEP=1
-NDCONV_EXE  = navdconv.exe
-TARGET_ARCH =
+CC         = x86_64-w64-mingw32-gcc
+CFLAGS     = -O3 -std=gnu99 -DMINGW_HAS_SECURE_API -DCOMPAT_STRCASESTR=1 -DCOMPAT_STRERROR_R=1 -DCOMPAT_STRNDUP=1 -DCOMPAT_STRSEP=1
+NDCONV_EXE = navdconv.exe
+TARGETARCH =
 
 all:
-	$(MAKE) -f Makefile CC="$(CC)" CFLAGS="$(CFLAGS)" TARGET_ARCH="$(TARGET_ARCH)" NDCONV_EXE="$(NDCONV_EXE)" navdconv
+	$(MAKE) -f Makefile CC="$(CC)" CFLAGS="$(CFLAGS)" TARGETARCH="$(TARGETARCH)" NDCONV_EXE="$(NDCONV_EXE)" navdconv
 
 .PHONY: clean
 clean:

src/plugins/navP/NVPchandlers.h
@@ -1,0 +1,29 @@
+/*
+ * NVPchandlers.h
+ *
+ * This file is part of the navdtools source code.
+ *
+ * (C) Copyright 2015 Timothy D. Walker and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU General Public License (GPL) version 2
+ * which accompanies this distribution (LICENSE file), and is also available at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * Contributors:
+ *     Timothy D. Walker
+ */
+
+#ifndef NVP_CHANDLERS_H
+#define NVP_CHANDLERS_H
+
+void* nvp_chandlers_init  (void                    );
+int   nvp_chandlers_reset (void   *chandler_context);
+int   nvp_chandlers_update(void   *chandler_context);
+int   nvp_chandlers_close (void **_chandler_context);
+
+#endif /* NVP_CHANDLERS_H */

src/plugins/navP/NVPplugin.c
@@ -1,0 +1,160 @@
+/*
+ * NVPplugin.c
+ *
+ * This file is part of the navdtools source code.
+ *
+ * (C) Copyright 2015 Timothy D. Walker and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU General Public License (GPL) version 2
+ * which accompanies this distribution (LICENSE file), and is also available at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * Contributors:
+ *     Timothy D. Walker
+ */
+
+#include <stdarg.h>
+#include <stdlib.h>
+#include <string.h>
+
+#include "XPLM/XPLMDefs.h"
+#include "XPLM/XPLMPlanes.h"
+#include "XPLM/XPLMPlugin.h"
+#include "XPLM/XPLMUtilities.h"
+
+#include "common/common.h"
+
+#include "NVPchandlers.h"
+
+/* Logging callback */
+static int log_with_sdk(const char *format, va_list ap);
+
+/* Miscellaneous data */
+void *chandler_context = NULL;
+
+#if IBM
+#include <windows.h>
+BOOL APIENTRY DllMain(HANDLE hModule,
+                      DWORD  ul_reason_for_call,
+                      LPVOID lpReserved)
+{
+    switch (ul_reason_for_call)
+    {
+        case DLL_PROCESS_ATTACH:
+        case DLL_THREAD_ATTACH:
+        case DLL_THREAD_DETACH:
+        case DLL_PROCESS_DETACH:
+            break;
+    }
+    return TRUE;
+}
+#endif
+
+PLUGIN_API int XPluginStart(char *outName,
+                            char *outSig,
+                            char *outDesc)
+{
+    strncpy(outName, "navP",                 255);
+    strncpy(outSig,  "Rodeo314.navP",        255);
+    strncpy(outDesc, "Miscellaneous stuff.", 255);
+
+    /* Initialize command handling context */
+    if ((chandler_context = nvp_chandlers_init()) == NULL)
+    {
+        return 0;
+    }
+
+#if APL
+    /* use native (POSIX) paths under OS X */
+    XPLMEnableFeature("XPLM_USE_NATIVE_PATHS", 1);
+#endif
+
+    /* all good */
+    XPLMDebugString("navP [info]: XPluginStart OK\n"); return 1;
+}
+
+PLUGIN_API void XPluginStop(void)
+{
+    if (chandler_context)
+    {
+        nvp_chandlers_close(&chandler_context);
+    }
+}
+
+PLUGIN_API int XPluginEnable(void)
+{
+    /* set ndt_log callback so we write everything to the X-Plane log */
+    ndt_log_set_callback(&log_with_sdk);
+
+    /* all good */
+    XPLMDebugString("navP [info]: XPluginEnable OK\n"); XPLMSpeakString("nav P OK"); return 1;
+}
+
+PLUGIN_API void XPluginDisable(void)
+{
+    /* unset ndt_log callback */
+    ndt_log_set_callback(NULL);
+
+    /* reset command handlers */
+    nvp_chandlers_reset(chandler_context);
+
+    /* all good */
+    XPLMDebugString("navP [info]: XPluginDisable OK\n");
+}
+
+PLUGIN_API void XPluginReceiveMessage(XPLMPluginID inFromWho,
+                                      long         inMessage,
+                                      void        *inParam)
+{
+    switch (inMessage)
+    {
+        case XPLM_MSG_PLANE_CRASHED:
+            break;
+
+        case XPLM_MSG_PLANE_LOADED:
+            if (inParam == XPLM_USER_AIRCRAFT)
+            {
+                nvp_chandlers_reset(chandler_context); // user's plane changed
+            }
+            break;
+
+        case XPLM_MSG_AIRPORT_LOADED:
+            break;
+
+        case XPLM_MSG_SCENERY_LOADED:
+            break;
+
+        case XPLM_MSG_AIRPLANE_COUNT_CHANGED:
+            break;
+
+        case XPLM_MSG_PLANE_UNLOADED:
+            break;
+
+        case XPLM_MSG_WILL_WRITE_PREFS:
+            break;
+
+        case XPLM_MSG_LIVERY_LOADED:
+            if (inParam == XPLM_USER_AIRCRAFT)
+            {
+                nvp_chandlers_update(chandler_context); // custom plugins loaded
+            }
+            break;
+
+        default:
+            break;
+    }
+}
+
+static int log_with_sdk(const char *format, va_list ap)
+{
+    int ret;
+    char string[1024];
+    ret = vsnprintf(string, sizeof(string), format, ap);
+    XPLMDebugString(string);
+    return ret;
+}

commit e5bc18a46fb90b101871a2efc0c568407a80a7ee

navdconv: fix print_version().
This was broken in f072c3e94111857099b79daee9e752cdd08fec7e

Makefile
@@ -34,16 +34,16 @@ NVP_OBJECTS = $(addsuffix .o,$(basename $(notdir $(NVP_SOURCES))))
 all: navdconv navp
 
 navp: nvpobj libobj comobj compat wmmobj
-	$(CC) $(NVP_XPLUGIN) $(SDKLDPATHS) $(SDKLDLINKS) $(SDKINCLUDE) $(NDTINCLUDE) $(NVP_DEFINES) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NAVP_XPDLL) $(NVP_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
+	$(CC) $(NVP_XPLUGIN) $(SDKLDPATHS) $(SDKLDLINKS) $(SDKINCLUDE) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NAVP_XPDLL) $(NVP_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
 
 nvpobj: $(NVP_SOURCES) $(NVP_HEADERS)
 	$(CC) $(SDKINCLUDE) $(NDTINCLUDE) $(NVP_DEFINES) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(NVP_SOURCES)
 
 navdconv: ndcobj libobj comobj compat wmmobj
-	$(CC) $(NDTINCLUDE) $(NDC_DEFINES) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NDCONV_EXE) $(NDC_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
+	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(LDFLAGS) $(TARGETARCH) -o $(NDCONV_EXE) $(NDC_OBJECTS) $(LIB_OBJECTS) $(COM_OBJECTS) $(CPT_OBJECTS) $(WMM_OBJECTS) $(LDLIBS)
 
 ndcobj: $(NDC_SOURCES)
-	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(NDC_SOURCES)
+	$(CC) $(NDTINCLUDE) $(NDC_DEFINES) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(NDC_SOURCES)
 
 libobj: $(LIB_SOURCES) $(LIB_HEADERS)
 	$(CC) $(NDTINCLUDE) $(CFLAGS) $(CPPFLAGS) $(TARGETARCH) -c $(LIB_SOURCES)