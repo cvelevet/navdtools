cb64fdc0addc1a1f98e986e4cb2973e1f3d7ce63
Tim Walker <tdskywalker@gmail.com>
2016-07-31 02:47:13 +0200

    1

---
+++ src/plugins/navP/YFSmain.c
@@ -0,0 +1,55 @@
+/*
+ * YFSmain.c
+ *
+ * This file is part of the navdtools source code.
+ *
+ * (C) Copyright 2016 Timothy D. Walker and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU General Public License (GPL) version 2
+ * which accompanies this distribution (LICENSE file), and is also available at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * Contributors:
+ *     Timothy D. Walker
+ */
+
+#include <stdlib.h>
+
+#include "YFSmain.h"
+
+typedef void yfms_context;
+
+void* yfs_main_init(void)
+{
+    yfms_context *yfms = calloc(1, sizeof(yfms_context));
+    if (!yfms)
+    {
+        return NULL;
+    }
+
+    /* all good */
+    return yfms;
+
+fail:
+    yfs_main_close((void**)&yfms);
+    return NULL;
+}
+
+int yfs_main_close(void **_yfms_context)
+{
+    yfms_context *yfms = *_yfms_context;
+    *_yfms_context = NULL;
+    if (!yfms)
+    {
+        return -1;
+    }
+
+    /* all good */
+    free(yfms);
+    return 0;
+}
---
+++ src/plugins/navP/YFSmain.h
@@ -0,0 +1,27 @@
+/*
+ * YFSmain.h
+ *
+ * This file is part of the navdtools source code.
+ *
+ * (C) Copyright 2016 Timothy D. Walker and others.
+ *
+ * All rights reserved. This program and the accompanying materials are made
+ * available under the terms of the GNU General Public License (GPL) version 2
+ * which accompanies this distribution (LICENSE file), and is also available at
+ * http://www.gnu.org/licenses/gpl-2.0.html
+ *
+ * This program is distributed in the hope that it will be useful, but WITHOUT
+ * ANY WARRANTY; without even the implied warranty of MERCHANTABILITY or FITNESS
+ * FOR A PARTICULAR PURPOSE. See the GNU General Public License for more details.
+ *
+ * Contributors:
+ *     Timothy D. Walker
+ */
+
+#ifndef YFS_MAIN_H
+#define YFS_MAIN_H
+
+void* yfs_main_init (void                );
+int   yfs_main_close(void **_yfms_context);
+
+#endif /* YFS_MAIN_H */
