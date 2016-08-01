3b2ed3c84390572fb89ccbf6ed31c87cfa8fb85d
Tim Walker <tdskywalker@gmail.com>
2016-08-02 01:09:23 +0200

    47

--- src/plugins/navP/YFSmenu.c
+++ src/plugins/navP/YFSmenu.c
@@ -80,10 +80,10 @@ void yfs_idnt_pageopen(yfms_context *yfms)
     sprintf(yfms->mwindow.screen.text[0], "%*s", (1 + YFS_DISPLAY_NUMC + len) / 2, buf);
 
     /* line 1: header (white, offset 1 right) */
-    yfs_main_rline(yfms, 1, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[1], "%s", " ENG");
+    yfs_main_rline(yfms,  1, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[ 1], "%s", " ENG");
 
     /* line 2: engine count & type (green) */
-    yfs_main_rline(yfms, 2, COLR_IDX_GREEN);
+    yfs_main_rline(yfms,  2, COLR_IDX_GREEN);
     XPLMGetDatavi (yfms->xpl.acf_en_type, &len, 0, 1);
     switch (len)
     {
@@ -99,18 +99,33 @@ void yfs_idnt_pageopen(yfms_context *yfms)
     }
 
     /* line 3: header (white, offset 1 right) */
-    yfs_main_rline(yfms, 3, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[3], "%s", " ACTIVE DATA BASE");
+    yfs_main_rline(yfms,  3, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[ 3], "%s", " ACTIVE DATA BASE");
 
     /* line 4: navigation database information (blue) */
-    yfs_main_rline(yfms, 4, COLR_IDX_BLUE);
+    yfs_main_rline(yfms,  4, COLR_IDX_BLUE);
     sprintf(yfms->mwindow.screen.text[4], "%.10s", yfms->ndt.ndb->info.idnt);
 
     /* line 5: header (white, offset 1 right) */
-    yfs_main_rline(yfms, 5, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[5], "%s", " SECOND DATA BASE");
+    yfs_main_rline(yfms,  5, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[ 5], "%s", " SECOND DATA BASE");
 
     /* line 6: navigation database information (blue) */
-    yfs_main_rline(yfms, 6, COLR_IDX_BLUE);  sprintf(yfms->mwindow.screen.text[6], "%s", "NONE");
+    yfs_main_rline(yfms,  6, COLR_IDX_BLUE);  sprintf(yfms->mwindow.screen.text[ 6], "%s", "NONE");
+
+#ifndef NDT_VERSION
+#define NDT_VERSION "Unknown"
+#endif
+    /* line 9: header (white, offset 1 right) */
+    yfs_main_rline(yfms,  9, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[ 9], "%s", " VERSION");
+
+    /* line 10: version information (blue) */
+    yfs_main_rline(yfms, 10, COLR_IDX_BLUE);  sprintf(yfms->mwindow.screen.text[10], "%s", NDT_VERSION);
+
+    /* line 11: header (white, offset 1 right) */
+    yfs_main_rline(yfms, 11, COLR_IDX_WHITE); sprintf(yfms->mwindow.screen.text[11], "%s", " IDLE/PERF");
+
+    /* line 12: version information (green) */
+    yfs_main_rline(yfms, 12, COLR_IDX_GREEN); sprintf(yfms->mwindow.screen.text[12], "%s", "+0.0/+0.0");
 
     /* all good */
-    return;//fixme
+    return;
 }
