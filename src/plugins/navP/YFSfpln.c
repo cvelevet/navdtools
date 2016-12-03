7b34c480a664fcfe98088bc49023eab1655814ee
Tim Walker <tdskywalker@gmail.com>
2016-12-03 01:30:23 +0100

    YFMS: FPLN: more parts.

--- src/plugins/navP/YFSfpln.c
+++ src/plugins/navP/YFSfpln.c
@@ -34,6 +34,7 @@
 static void yfs_lsk_callback_fpln(yfms_context *yfms, int key[2],                intptr_t refcon);
 static void fpl_print_leg_generic(yfms_context *yfms, int row,                ndt_route_leg *leg);
 static void fpl_print_airport_rwy(yfms_context *yfms, int row, ndt_airport *apt, ndt_runway *rwy);
+static int  fpl_getindex_for_line(yfms_context *yfms, int line                                  );
 
 void yfs_fpln_pageopen(yfms_context *yfms)
 {
@@ -94,53 +95,63 @@ void yfs_fpln_pageupdt(yfms_context *yfms)
                    yfms->ndt.flp.rte->arr.rwy ? yfms->ndt.flp.rte->arr.rwy->info.idnt : "");
 
     /* two lines per leg (header/course/distance, then name/constraints) */
-    int legnum = ndt_list_count(yfms->data.fpln.legs);
-    for (int i = -1; i < 4; i++)
+    for (int i = 0; i < 5; i++)
     {
-        int index = yfms->data.fpln.lg_idx + yfms->data.fpln.ln_off + i, actual_leg = 0;
-        if (index < -1 || index >= legnum)
-        {
-            while (index > legnum)
-            {
-                index -= legnum + 4; // legnum + 1 == -3, legnum + 2 == -2, etc.
-            }
-            while (index < -4)
-            {
-                index += legnum + 4; // -5 == legnum - 1, -6 == legnum - 2, etc.
-            }
-            if (index == legnum)
-            {
-                index = -4;
-            }
-        }
-        switch (index)
+        int have_waypt = 0, index;
+        switch ((index = fpl_getindex_for_line(yfms, i)))
         {
             case -3:
-                yfs_printf_lft       (yfms, (2 * (i + 2)), 0, COLR_IDX_WHITE, "%s",    "----- END OF F-PLN -----");
+                yfs_printf_lft       (yfms, (2 * (i + 1)), 0, COLR_IDX_WHITE, "%s",    "----- END OF F-PLN -----");
                 break;
             case -2:
-                yfs_printf_lft       (yfms, (2 * (i + 2)), 0, COLR_IDX_WHITE, "%s",    "----- NO ALTN FPLN -----");
+                yfs_printf_lft       (yfms, (2 * (i + 1)), 0, COLR_IDX_WHITE, "%s",    "----- NO ALTN FPLN -----");
                 break;
             case -1: // departure airport
-                fpl_print_airport_rwy(yfms, (2 * (i + 2)), yfms->ndt.flp.rte->dep.apt, yfms->ndt.flp.rte->dep.rwy);
-                actual_leg = 1; break;//fixme
+                fpl_print_airport_rwy(yfms, (2 * (i + 1)), yfms->ndt.flp.rte->dep.apt, yfms->ndt.flp.rte->dep.rwy);
+                have_waypt = 1; break;
             case -4: // arrival airport
-                fpl_print_airport_rwy(yfms, (2 * (i + 2)), yfms->ndt.flp.rte->arr.apt, yfms->ndt.flp.rte->arr.rwy);
-                actual_leg = 1; break;
+                fpl_print_airport_rwy(yfms, (2 * (i + 1)), yfms->ndt.flp.rte->arr.apt, yfms->ndt.flp.rte->arr.rwy);
+                have_waypt = 1; break;
             default: // regular leg
-                fpl_print_leg_generic(yfms, (2 * (i + 2)),             ndt_list_item(yfms->data.fpln.legs, index));
-                actual_leg = 1; break;
+                fpl_print_leg_generic(yfms, (2 * (i + 1)),             ndt_list_item(yfms->data.fpln.legs, index));
+                have_waypt = 1; break; // TODO: check for discontinuity legs too
         }
-        if (actual_leg)
+        if (have_waypt)
         {
             switch (i)
             {
-                case -1: // column headers
-                    yfs_printf_lft(yfms, ((2 * (i + 2)) - 1), 0, COLR_IDX_WHITE, "%s", "        TIME  SPD/ALT");
+                case 0: // column headers
+                    yfs_printf_lft(yfms, ((2 * (i + 1)) - 1), 0, COLR_IDX_WHITE, "%s", "        TIME  SPD/ALT");
                     break;
                 default: // course, distance
-                    //fixme
+                {
+                    if (index == -1)
+                    {
+                        break; // departure: we have a waypoint, but no leg
+                    }
+                    ndt_route_leg *leg = index == -4 ? yfms->ndt.flp.rte->arr.last.rleg : ndt_list_item(yfms->data.fpln.legs, index);
+                    double distance_nm = (double)ndt_distance_get(leg->dis, NDT_ALTUNIT_ME) / 1852.;
+                    switch(leg->rsg->type)
+                    {
+                        case NDT_RSTYPE_PRC://fixme
+//                          if (leg->src && leg->dst) // we have bearing information, should we use it???
+                            break;
+                        case NDT_RSTYPE_AWY: // TODO: also print bearing next to airway identifier???
+                            yfs_printf_lft(yfms, ((2 * (i + 1)) - 1), 0, COLR_IDX_WHITE, " C%03.0lf", leg->awyleg->awy->info.idnt);
+                            yfs_printf_rgt(yfms, ((2 * (i + 1)) - 1), 0, COLR_IDX_WHITE, "%.0lf     ", distance_nm);
+                            break;
+                        case NDT_RSTYPE_DCT:
+                        default:
+                            yfs_printf_lft(yfms, ((2 * (i + 1)) - 1), 0, COLR_IDX_WHITE, " C%03.0lf", leg->imb);
+                            yfs_printf_rgt(yfms, ((2 * (i + 1)) - 1), 0, COLR_IDX_WHITE, "%.0lf     ", distance_nm);
+                            break;
+                    }
+                    if (i == 1)
+                    {
+                        yfs_printf_rgt(yfms, ((2 * (i + 1)) - 1), 3, COLR_IDX_WHITE, "%s", "NM");
+                    }
                     break;
+                }
             }
         }
     }
@@ -151,6 +162,11 @@ void yfs_fpln_pageupdt(yfms_context *yfms)
 
 static void yfs_lsk_callback_fpln(yfms_context *yfms, int key[2], intptr_t refcon)//fixme
 {
+    if (key[0] == 0 && key[1] != 5) // insert waypoint or open lateral rev. page
+    {
+        int index = fpl_getindex_for_line(yfms, key[1]);
+        //fixme
+    }
     /* all good */
     return;
 }
@@ -169,4 +185,26 @@ static void fpl_print_airport_rwy(yfms_context *yfms, int row, ndt_airport *apt,
     }
     yfs_printf_lft(yfms, row, 0, COLR_IDX_GREEN, "%-7s ----  ---/%6d", apt->info.idnt,
                    ndt_distance_get(ndt_position_getaltitude(apt->coordinates), NDT_ALTUNIT_FT)); return;
+}
+
+static int fpl_getindex_for_line(yfms_context *yfms, int line)
+{
+    int legct = ndt_list_count(yfms->data.fpln.legs);
+    int index = yfms->data.fpln.lg_idx + yfms->data.fpln.ln_off + line - 1;
+    if (index < -1 || index >= legct)
+    {
+        while (index > legct)
+        {
+            index -= legct + 4; // legct + 1 == -3, legct + 2 == -2, etc.
+        }
+        while (index < -4)
+        {
+            index += legct + 4; // -5 == legct - 1, -6 == legct - 2, etc.
+        }
+        if (index == legct)
+        {
+            index = -4;
+        }
+    }
+    return index;
 }
