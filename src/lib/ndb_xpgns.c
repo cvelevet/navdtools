8a3c47470098fbbd736de7893eaaeb300846d8dc
Tim Walker <tdskywalker@gmail.com>
2017-07-08 08:58:08 +0200

    ndb_xpgns: fix silly log error (caused by unitialized variable).

--- src/lib/ndb_xpgns.c
+++ src/lib/ndb_xpgns.c
@@ -859,6 +859,7 @@ ndt_airport* ndt_ndb_xpgns_navdata_init_airport(ndt_navdatabase *ndb, ndt_airpor
     if (apt->allprocs)
     {
         // success (already parsed)
+        err = 000;
         ret = apt;
         goto  end;
     }
@@ -910,6 +911,7 @@ ndt_airport* ndt_ndb_xpgns_navdata_init_airport(ndt_navdatabase *ndb, ndt_airpor
     }
 
     // success
+    err = 000;
     ret = apt;
 
 end:
