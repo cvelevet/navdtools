6ed3fab68cc3b90cd05e20736930a14384066071
Tim Walker <tdskywalker@gmail.com>
2017-07-20 02:18:31 +0200

    navP: these are floats, you know :-)

--- src/plugins/navP/NVPchandlers.c
+++ src/plugins/navP/NVPchandlers.c
@@ -3847,15 +3847,15 @@ static int first_fcall_do(chandler_context *ctx)
                 {
                     if (!STRN_CASECMP_AUTO(ctx->desc, "Pipistrel Panthera"))
                     {
-                        _DO(0, XPLMSetDatai,   0, "sim/cockpit2/autopilot/airspeed_is_mach");
-                        _DO(0, XPLMSetDatai, 125, "sim/cockpit2/autopilot/airspeed_dial_kts_mach");
+                        _DO(0, XPLMSetDatai,      0, "sim/cockpit2/autopilot/airspeed_is_mach");
+                        _DO(0, XPLMSetDataf, 125.0f, "sim/cockpit2/autopilot/airspeed_dial_kts_mach");
                     }
                     if (!STRN_CASECMP_AUTO(ctx->desc, "Epic E1000")   ||
                         !STRN_CASECMP_AUTO(ctx->desc, "Epic Victory") ||
                         !STRN_CASECMP_AUTO(ctx->desc, "The Eclipse 550"))
                     {
-                        _DO(0, XPLMSetDatai,   0, "sim/cockpit2/autopilot/airspeed_is_mach");
-                        _DO(0, XPLMSetDatai, 175, "sim/cockpit2/autopilot/airspeed_dial_kts_mach");
+                        _DO(0, XPLMSetDatai,      0, "sim/cockpit2/autopilot/airspeed_is_mach");
+                        _DO(0, XPLMSetDataf, 175.0f, "sim/cockpit2/autopilot/airspeed_dial_kts_mach");
                     }
                 }
             }
