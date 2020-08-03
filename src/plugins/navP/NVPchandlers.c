c1218c3e8224485459f42f99e8cf89540200ef89
Tim Walker <tdskywalker@gmail.com>
2020-08-04 01:12:29 +0200

    Note to self (F5).

--- src/plugins/navP/NVPchandlers.c
+++ src/plugins/navP/NVPchandlers.c
@@ -3788,7 +3788,7 @@ static int chandler_mcdup(XPLMCommandRef inCommand, XPLMCommandPhase inPhase, vo
                                 if (!STRN_CASECMP_AUTO(outp_descr, "Console"))
                                 {
                                     // F5 defaults to mixture_down and is free on Apple's KBs
-                                    XPLMSetHotKeyCombination(hot_key, XPLM_VK_F5, xplm_UpFlag);
+                                    XPLMSetHotKeyCombination(hot_key, XPLM_VK_F5, xplm_UpFlag);//fixme use other key
                                     continue;
                                 }
                                 if (!STRN_CASECMP_AUTO(outp_descr, "mcdu 2"))
