b4b4d3b7bf3ab2a7e03b1d73d99b3cfdb2bad86f
Tim Walker <tdskywalker@gmail.com>
2021-01-10 00:10:56 +0100

    ACFvolumes: ToLiSS: X-Plane 10 vs. 11 (FMOD native sounds) paths…

--- src/plugins/navP/ACFvolumes.c
+++ src/plugins/navP/ACFvolumes.c
@@ -361,7 +361,23 @@ void acf_volume_set(acf_volume_context *ctx, float volume, acf_type type)
         {
             case ACF_TYP_A319_TL:
             case ACF_TYP_A321_TL:
-                XPLMSetDataf(ctx->custom.tlss.master, sqrtf(volume));
+                if (ctx->x_plane_v11)
+                {
+                    XPLMSetDataf(ctx->sound.xp11.mas, 2.0f * volume);
+                    XPLMSetDataf(ctx->sound.xp11.cop, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.eng, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.env, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.ext, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.fan, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.grd, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.inn, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.prp, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.uiv, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.wrn, 0.5f);
+                    XPLMSetDataf(ctx->sound.xp11.wxr, 0.5f);
+                    break;
+                }
+                XPLMSetDataf(ctx->custom.tlss.master, sqrtf(sqrtf(volume)));
                 break;
 
             case ACF_TYP_A320_FF:
